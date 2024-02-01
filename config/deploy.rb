e89819bb16e3b397227d6c1c4a60c392f1168ad2
sediqwe <sediqwe@gmail.com>
2024-02-01 18:13:10 +0100

    Games

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :repo_url, "https://github.com/Sediqwe/m4.git"
 set :branch, "main"
 set :deploy_to, "/var/www/monsterv4"
 set :keep_releases, 3
-set :migration_command, 'db:create'
+set :migration_command, 'db:migrate'
 set :conditionally_migrate, true
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "games#index"
+  resources :games
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
